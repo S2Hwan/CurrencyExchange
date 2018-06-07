035417e93aa41e884b5a7f601b509dd6c7a01f62
s2h <ssh92kr0410@naver.com>
2018-06-08 00:13:38 +0900

    DetailCV작성

--- CurrencyExchange/DetailViewController.swift
+++ CurrencyExchange/DetailViewController.swift
@@ -9,27 +9,38 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet weak var cur_nmLabel: UILabel!
+    @IBOutlet weak var cur_unitLabel: UILabel!
+    @IBOutlet weak var deal_bas_rLabel: UILabel!
+    @IBOutlet weak var inputTextFiled: UITextField!
+    
+     var currency : CurrencyData?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cur_nmLabel.text = currency?.cur_nm
+        cur_unitLabel.text = currency?.cur_unit
+        deal_bas_rLabel.text = currency?.deal_bas_r
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func exchangeButton(_ sender: UIButton) {
+        
+        let inputData = inputTextFiled.text
+        let rate = deal_bas_rLabel.text
+        
+        deal_bas_rLabel.text = String(Double(inputData!)! * Double(rate!)!)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func resetButton(_ sender: UIButton) {
+        
+        deal_bas_rLabel.text = currency?.deal_bas_r
+        inputTextFiled.text = ""
     }
-    */
-
+    
 }
