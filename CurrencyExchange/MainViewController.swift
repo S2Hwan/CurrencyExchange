07acb665d304f3b67aeba09a94a26156571caac7
s2h <ssh92kr0410@naver.com>
2018-06-08 00:02:04 +0900

    MainVC 완성

--- CurrencyExchange/MainViewController.swift
+++ CurrencyExchange/MainViewController.swift
@@ -8,28 +8,64 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var currency = [CurrencyData]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        downloadJSON()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currency.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = currency[indexPath.row].cur_nm
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToDetail", sender: self)
+    }
+    
+   
+    
+    
+    
+    
+    func downloadJSON() {
+        
+        let jsonURL = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=OHtOxoabzlAmw7iv7WH8QkWVgW7bVH51&searchdate=20180102&data=AP01"
+        let url = URL(string: jsonURL)
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error == nil {
+                do {
+                    self.currency = try JSONDecoder().decode([CurrencyData].self, from: data!)
+                    
+                    DispatchQueue.main.sync {
+                        self.tableView.reloadData()
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+        
+        
+    
+    }
 
+ 
 }
