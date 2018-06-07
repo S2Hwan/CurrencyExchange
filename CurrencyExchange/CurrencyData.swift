50e4ceaabf823a86bfdc2764b359c1204c76c1ee
s2h <ssh92kr0410@naver.com>
2018-06-07 23:54:01 +0900

    data 작성

--- CurrencyExchange/CurrencyData.swift
+++ CurrencyExchange/CurrencyData.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+struct CurrencyData : Decodable {
+    let cur_unit : String // 통화코드
+    let cur_nm : String // 국가 / 통화명
+    let bkpr : String // 장부가격
+    let deal_bas_r : String // 매매 기준율
+    
+}
+
